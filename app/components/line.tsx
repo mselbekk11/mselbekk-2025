0187b3540ebab8bf12b82ae5785cb1b29e24309e
mselbekk11 <mselbekk11@gmail.com>
2026-01-12 19:51:18 -0800

    updates

--- app/components/line.tsx
+++ app/components/line.tsx
@@ -1,5 +1,19 @@
+'use client';
+
+import { motion } from 'motion/react';
+
 export default function Line() {
   return (
-    <div className='block h-px w-full bg-[length:4px_1px] bg-bottom bg-repeat-x [--ui-border-color:theme(colors.muted)] [background-image:linear-gradient(90deg,var(--ui-border-color)_1px,transparent_1px)]'></div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{
+        duration: 0.5,
+        delay: 0.6,
+        ease: 'easeIn',
+      }}
+    >
+      <div className='block h-px w-full bg-[length:4px_1px] bg-bottom bg-repeat-x [--ui-border-color:theme(colors.muted)] [background-image:linear-gradient(90deg,var(--ui-border-color)_1px,transparent_1px)]'></div>
+    </motion.div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -95,7 +95,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang='en' className='dark'>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} ${stixTwoText.variable} antialiased min-h-screen max-w-screen p-4 md:p-6 flex flex-col`}
       >
