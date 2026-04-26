e67dc9b9bfd73c19609dce176720fc1afe7ab4c9
mselbekk11 <mselbekk11@gmail.com>
2026-04-25 18:55:44 -0700

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -108,6 +108,11 @@ export default function RootLayout({
         <div className='block md:hidden mt-auto pt-16 text-4xl text-muted opacity-20 font-stix-two-text'>
           Morgan Selbekk
         </div>
+        <script
+          src='http://localhost:3000/widget/embed.js'
+          data-chatbot-id='jh70ve6080azscxgq0003d6tvx843mgw'
+          async
+        ></script>
       </body>
       <Script src='https://scripts.simpleanalyticscdn.com/latest.js' />
     </html>
